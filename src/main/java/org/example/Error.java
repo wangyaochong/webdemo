7b9e91915c501139987d42192eb7f9bd49477720
wyc <wyc>
2022-11-05 17:46:30 +0800

    添加sonar脚本

---
+++ src/main/java/org/example/Error.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Error {
+    public static void main(String[] args) {
+        int i=1/0;
+        int a=1;
+        a=2;
+        a=3;
+        while(true){
+            System.out.println("hello");
+        }
+    }
+}
